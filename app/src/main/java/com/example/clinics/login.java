ac97e444aa8666912177af395db53ed475c79809
ibrahim <himahamed999@gmail.com>
2020-03-05 19:12:39 +0200

    11

--- app/src/main/java/com/example/clinics/login.java
+++ app/src/main/java/com/example/clinics/login.java
@@ -1,12 +1,11 @@
 package com.example.clinics;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
 
 import java.util.ArrayList;
 
@@ -28,26 +27,26 @@ public class login extends AppCompatActivity {
         mRecyclerView = findViewById ( R.id.recyclerView );
         mlayoutManager = new LinearLayoutManager ( this );
         mAdapter = new wordadapter ( list );
-        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setHasFixedSize ( true );
         mRecyclerView.setLayoutManager ( mlayoutManager );
         mRecyclerView.setAdapter ( mAdapter );
-        mAdapter.SetOnItemClickListener(new wordadapter.OnItemClickListener() {
+        mAdapter.SetOnItemClickListener ( new wordadapter.OnItemClickListener () {
             @Override
             public void onItemClick(int position) {
-                String list1=list.get(position).getmTextV1();
-                Intent in=new Intent(login.this,Detalis .class);
-                in.putExtra("name",list1);
-                startActivity(in);
+                String list1 = list.get ( position ).getmTextV1 ();
+                Intent in = new Intent ( login.this, Detalis.class );
+                in.putExtra ( "name", list1 );
+                startActivity ( in );
 
 
             }
-        });
+        } );
     }
 
     public void CreateArrayList() {
         list = new ArrayList<> ();
-        list.add ( new wordclass ( R.drawable.heart, "العيادات","1" ) );
+        list.add ( new wordclass ( R.drawable.heart, "العيادات", "1" ) );
         list.add ( new wordclass ( R.drawable.eye, "العيون", "2" ) );
-        list.add ( new wordclass ( R.drawable.b, "التجميل" ,"3" ));
+        list.add ( new wordclass ( R.drawable.b, "التجميل", "3" ) );
     }
 }

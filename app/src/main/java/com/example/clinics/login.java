18b41cad0b6204cd50acedc5582c333fd793f86a
ibrahim <himahamed999@gmail.com>
2020-03-14 22:40:12 +0200

    11

--- app/src/main/java/com/example/clinics/login.java
+++ app/src/main/java/com/example/clinics/login.java
@@ -47,7 +47,6 @@ public class login extends AppCompatActivity {
                         startActivity ( new Intent ( login.this, beauty.class ) );
                         break;
                 }
-
             }
         } );
     }
